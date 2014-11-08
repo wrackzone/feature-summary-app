eb8d2ca0cb32f68c6c9889762e670f309b9dc680
Barry Mullan <mullan@gmail.com>
2014-11-08 10:07:18 -0500

    adding support for dependencies

--- App.js
+++ App.js
@@ -8,7 +8,8 @@ Ext.define('CustomApp', {
     config: {
         defaultSettings : {
             showTime : false,
-            showTasks : true
+            showTasks : true,
+            showDependencies : true
         }
     },
 
@@ -23,6 +24,11 @@ Ext.define('CustomApp', {
                 name: 'showTasks',
                 xtype: 'rallycheckboxfield',
                 label : "Selected to show Task columns"
+            },
+            {
+                name: 'showDependencies',
+                xtype: 'rallycheckboxfield',
+                label : "Selected to show Task columns"
             }
         ];
     },
@@ -31,8 +37,18 @@ Ext.define('CustomApp', {
         app = this;
         app.showTime = app.getSetting("showTime");
         app.showTasks = app.getSetting("showTasks");
+        app.showDependencies = app.getSetting("showDependencies");
+        app.releaseName = null;
 
-        console.log("Hello World!");
+        var timeboxScope = this.getContext().getTimeboxScope();
+        if (timeboxScope) {
+            app.releaseName = timeboxScope.getType() === 'iteration' ? 
+                record.get('Name') : null;
+        } else {
+            app.releaseName = "Release 1"
+        }
+        console.log("Release",app.releaseName);
+        
         this.addFeatureGrid();
     },
 
@@ -113,6 +129,45 @@ Ext.define('CustomApp', {
             return (!_.isUndefined(blockedSnapshots) && blockedSnapshots.length > 0) ? blockedSnapshots.length : "";
         }
     },
+
+    dependenciesColumn : {  
+        text: "Dependencies", width:100, 
+        renderer : function(value, metaData, record, rowIdx, colIdx, store, view) {
+            var snapshots = record.get("Dependencies");
+            if (snapshots!==undefined)
+                console.log(record.get("FormattedID"),snapshots.length,
+                    snapshots.length>0 ? snapshots[0].get("FormattedID") : "",
+                    snapshots.length>0 ? _.map(snapshots[0].get("PredStories"),function(s){return s.get("FormattedID");}):"");
+            return (!_.isUndefined(snapshots) && snapshots.length > 0) ? snapshots.length : "";
+        }
+    },
+
+    // creates a filter to return all releases with a specified set of names
+    createReleaseFilter : function(releaseNames) {
+
+        console.log("releaseNames",releaseNames.split(","));
+
+        var filter = null;
+
+        _.each( releaseNames.split(","), function( releaseName, i ) {
+            if (releaseName !== "") {
+                var f = Ext.create('Rally.data.wsapi.Filter', {
+                        property : 'Release.Name', operator : '=', value : app.trimString(releaseName) }
+                );
+                filter = (i===0) ? f : filter.or(f);
+            }
+        });
+
+        console.log("Release Filter:",filter.toString());
+        return filter;
+
+    },
+
+    // remove leading and trailing spaces
+    trimString : function (str) {
+        return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+    },
+
     
     addFeatureGrid : function() {
         // var viewport = Ext.create('Ext.Viewport');
@@ -135,32 +190,56 @@ Ext.define('CustomApp', {
                 columnCfgs.push(app.taskToDoColumn);
                 columnCfgs.push(app.taskActualsColumn);
             }
+            if (app.showDependencies) {
+                columnCfgs.push(app.dependenciesColumn);
+            }
 
             var grid = Ext.create('Rally.ui.grid.Grid',
                 {
                  xtype: 'rallygrid',
                  model: userStoryModel,
+                 storeConfig : {
+                    filters : app.releaseName !== null  ? [app.createReleaseFilter(app.releaseName)] : null
+                 },
                  listeners : {
                     load : function(items) {
                         console.log("load",items.data.items);
                         var features = items.data.items;
-                        async.map(features,app.getSnapshots, function(err,results) {
-                            _.each( features, function(feature,i){
-                                feature.set("Defects",results[i]);
-                            })
-                            async.map(features,app.getBlockedSnapshots, function(err,results) {
-                                _.each( features, function(feature,i){
-                                    feature.set("Blocked",results[i]);
-                                });
-                                async.map(features,app.getTaskSnapshots, function(err,results) {
-                                    _.each( features, function(feature,i){
-                                        if (results[i].length>0)
-                                            console.log(results[i]);
 
-                                        feature.set("Tasks",results[i]);
-                                    });
+                        async.series( [
+                            function(callback) {
+                                async.map(features,app.getDefectSnapshots, function(err,results) {
+                                    callback(null,results);
                                 });
-                            });
+                            },
+                            function(callback) {
+                                async.map(features,app.getBlockedSnapshots, function(err,results) {
+                                    callback(null,results);
+                                });
+                            },
+                            function(callback) {
+                                async.map(features,app.getTaskSnapshots, function(err,results) {
+                                    callback(null,results);
+                                });
+                            },
+                            function(callback) {
+                                async.map(features,app.getDependencySnapshots, function(err,results) {
+
+                                    async.mapSeries(results,app.readPredecessors,function(err,preds) {
+                                            callback(null,results);
+                                    })
+                                });
+                            }
+
+                        ], function(err,results) {
+                            // indices: 0 Defects, 1 Blocked, 2 Tasks, 3 Dependencies
+                            // console.log("results",err,results);
+                            _.each( features, function( feature,i){
+                                feature.set("Defects",      results[0][i]);
+                                feature.set("Blocked",      results[1][i]);
+                                feature.set("Tasks",        results[2][i]);
+                                feature.set("Dependencies", results[3][i]);
+                            })
                         });
                     }
                  },
@@ -176,8 +255,39 @@ Ext.define('CustomApp', {
         });
     }
     ,
+
+    readPredecessors : function( stories, callback) {
+
+        async.mapSeries(stories,
+            function(story,callback) {
+                async.mapSeries(story.get("Predecessors"),app.readStory,function(err,results) {
+                    var preds = _.flatten(results);
+                    story.set("PredStories",preds)
+                    callback(null,null);
+                });
+            },
+            function(err,stories) {
+                callback(null,null);
+            }
+        );
+    },
+
+    // read a single story based on id
+    readStory : function( id, callback) {
+         var configs = [
+            { 
+                model : "HierarchicalRequirement",
+                fetch : true,
+                filters : [{property:"ObjectID",operator:"=",value:id}]
+            }
+        ];
+
+        async.map( configs, app.wsapiQuery, function(err,results) {
+            callback(null,results[0]);
+        });
+    },
     
-    getSnapshots : function(record, callback) {
+    getDefectSnapshots : function(record, callback) {
 
         var that = this;
         var fetch = ['ObjectID','_UnformattedID','State','Priority','Severity','_ItemHierarchy','_TypeHierarchy'];
@@ -204,9 +314,7 @@ Ext.define('CustomApp', {
                 }
             }
         };
-
         var snapshotStore = Ext.create('Rally.data.lookback.SnapshotStore', storeConfig);
-
     },
 
     getBlockedSnapshots : function(record, callback) {
@@ -305,6 +413,40 @@ Ext.define('CustomApp', {
 
     },
 
+    getDependencySnapshots : function(record, callback) {
+
+        var that = this;
+        var fetch = ['ObjectID','Estimate','ToDo','Actuals','_ItemHierarchy','_TypeHierarchy','Predecessors','Successors','Name','FormattedID','ScheduleState','PlanEstimate'];
+        var hydrate = ['_TypeHierarchy','ScheduleState'];
+        
+        var find = {
+                '_TypeHierarchy' : { "$in" : ["HierarchicalRequirement"]} ,
+                '_ProjectHierarchy' : { "$in": [app.getContext().getProject().ObjectID] },
+                '__At' : 'current',
+                "_ItemHierarchy" : { "$in" : [record.get("ObjectID")]  },
+                "Predecessors" : { "$exists" : true }
+        };
+
+        var storeConfig = {
+            find : find,
+            autoLoad : true,
+            pageSize:1000,
+            limit: 'Infinity',
+            fetch: fetch,
+            hydrate: hydrate,
+            listeners : {
+                scope : this,
+                load: function(store, snapshots, success) {
+                    callback(null,snapshots);
+                }
+            }
+        };
+
+        var snapshotStore = Ext.create('Rally.data.lookback.SnapshotStore', storeConfig);
+
+    },
+
+
     timeEntryItems : function( refs, callback ) {
 
         var configs = [
@@ -357,8 +499,5 @@ Ext.define('CustomApp', {
         });
 
     },
-
-
-
     
 });
